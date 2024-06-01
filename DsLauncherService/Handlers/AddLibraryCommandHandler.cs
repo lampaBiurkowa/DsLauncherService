49451d4424a325a1f51f2fd9a8b8c8e1be6277f2
hnoj <mrocznaklawiatura@gmail.com>
2024-06-02 01:35:08 +0200

    01:35

---
+++ DsLauncherService/Handlers/AddLibraryCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using DibBase.Infrastructure;
+using DsLauncherService.Communication;
+using DsLauncherService.Storage;
+
+namespace DsLauncherService.Handlers;
+
+[Command("add-library")]
+internal class AddLibraryCommandHandler(Repository<Library> libraryRepo, Repository<Installed> installedRepo) : ICommandHandler
+{    
+    public async Task<Command> Handle(CommandArgs args, CancellationToken ct)
+    {
+        var libraryPath = args.Get<string>("library").Trim();
+        if (Directory.Exists(libraryPath) && !IsDirectoryEmpty(libraryPath))
+            return Command.Empty;
+        
+        await libraryRepo.InsertAsync(new() { Path = libraryPath }, ct);
+        await libraryRepo.CommitAsync(ct);
+
+        return Command.Empty;
+    }
+
+    static bool IsDirectoryEmpty(string path) => !Directory.EnumerateFileSystemEntries(path).Any();
+}
---
+++ DsLauncherService/Handlers/RemoveLibraryCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using DibBase.Infrastructure;
+using DsLauncherService.Communication;
+using DsLauncherService.Storage;
+
+namespace DsLauncherService.Handlers;
+
+[Command("remove-library")]
+internal class RemoveLibraryCommandHandler(Repository<Library> libraryRepo, Repository<Installed> installedRepo) : ICommandHandler
+{    
+    public async Task<Command> Handle(CommandArgs args, CancellationToken ct)
+    {
+        var libraryPath = args.Get<string>("library").Trim();
+        var library = (await libraryRepo.GetAll(restrict: x => x.Path == libraryPath, ct: ct)).FirstOrDefault();
+        if (library == null) return Command.Empty;
+
+        var isUsed = (await installedRepo.GetAll(restrict: x => x.LibraryId == library.Id, ct: ct)).Count != 0;
+        if (isUsed) return Command.Empty;
+
+        await libraryRepo.DeleteAsync(library.Id, ct);
+        await libraryRepo.CommitAsync(ct);
+
+        return Command.Empty;
+    }
+}
